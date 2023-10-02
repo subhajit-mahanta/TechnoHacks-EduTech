726807caf0aa1fbc72f40c9345a9f2c2b95bb26b
Subhajit Mahanta <100114009+subhajit-mahanta@users.noreply.github.com>
2023-10-03 01:18:24 +0530

    Create PasswordGenerator.java

---
+++ Task 2/PasswordGenerator.java
@@ -0,0 +1,28 @@
+import java.security.SecureRandom;
+
+public class PasswordGenerator {
+    private static final String CHAR_UPPER = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private static final String CHAR_LOWER = "abcdefghijklmnopqrstuvwxyz";
+    private static final String DIGITS = "0123456789";
+    private static final String SPECIAL_CHARS = "!@#$%^&*()-_+=<>?";
+
+    public static void main(String[] args) {
+        int length = 12; // Desired length of the password
+
+        String password = generateRandomPassword(length);
+        System.out.println("Generated Password: " + password);
+    }
+
+    public static String generateRandomPassword(int length) {
+        String characters = CHAR_UPPER + CHAR_LOWER + DIGITS + SPECIAL_CHARS;
+        SecureRandom random = new SecureRandom();
+
+        StringBuilder password = new StringBuilder();
+        for (int i = 0; i < length; i++) {
+            int randomIndex = random.nextInt(characters.length());
+            password.append(characters.charAt(randomIndex));
+        }
+
+        return password.toString();
+    }
+}
